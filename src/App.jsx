203b9d77633adbe7cf32862710dae38ea4dc4a25
Nagul <drenugadevidurai@email.com>
2026-04-12 21:15:53 +0530

    Fix signup route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage'
 import HomePage from './pages/HomePage';
 import ResultPage from './pages/ResultPage';
 import HistoryPage from './pages/HistoryPage';
@@ -14,7 +15,7 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="/signup" element={<LoginPage/>} />
+                <Route path="/signup" element={<SignupPage/>} />
                 <Route path="/" element={
                     <PrivateRoute>
                         <HomePage />
